a19ce4495bcfc4ee136790f86fbf2f72d14a7639
Felipe Vilela <felipe.vilela01@outlook.com>
2025-09-17 19:56:18 -0300

    fix: indentation correction suggested in the review

--- front/src/components/ui/Footer/index.tsx
+++ front/src/components/ui/Footer/index.tsx
@@ -3,7 +3,7 @@ import type { FC } from 'react';
 const Footer: FC = () => (
   <footer className="w-full bg-emerald-700 text-white shadow-md py-4 px-6 mt-auto">
       <div className="mb-2 md:mb-0 text-center items-center md:text-center">
-        <span className="font-semibold">Sportrent</span> &copy; {new Date().getFullYear()} - Aluguel de Quadras
+        <span className="font-semibold">Sportrent</span> &copy; { new Date().getFullYear() } - Aluguel de Quadras
       </div>
   </footer>
 );
